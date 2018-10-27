16ecc3ded6258748d47b7c7bedc2bebdd26359df
Carlos Fernando Arboleda Garces <arbofercho@gmail.com>
2018-10-27 11:36:35 -0500

    Fix eslint

---
+++ 4_DefaultRestSpread/2_rest.js
@@ -0,0 +1,15 @@
+
+/**
+ * 2. Try to refactor code below to es6 using rest
+ * https://github.com/lukehoban/es6features#default--rest--spread
+ */
+
+function getAllArgs(a, b) {
+    const args = Array.prototype.slice.call(arguments, getAllArgs.length);
+
+    console.log('My arguments', a, b);
+
+    return args;
+}
+
+console.log('Others', getAllArgs(1, 2, 3, { test: 'test' }));
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # es6-practice
 Practica de características de ES6
+
+## Ejecutar eslint:
+```shell
+cd ./es6-practice
+npm run eslint
+```
